c47dff446644eeda6c021dc59c46bcf265af22dc
Mattia Forti <mattia.forti2@studio.unibo.it>
2024-11-04 18:01:43 +0100

    [ADD] Created lib file and utils file

---
+++ project/src/main/utils.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+constexpr auto SCREEN_WIDTH = 1920;
+constexpr auto SCREEN_HEIGHT = 1080;
+
+constexpr auto WIDTH = 800;
+constexpr auto HEIGHT = 800;
+
+constexpr auto PI = 3.14159265358979323846;
+
+constexpr auto BACKGROUND_COLOR = vec4(0.0f);
